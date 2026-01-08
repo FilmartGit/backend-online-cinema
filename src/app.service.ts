10869a0c2d8348f38d4dd0ae66dc4f6ae2bd2097
Ivan <ivan.filmart@gmail.com>
2026-01-08 16:29:52 +0300

    описание проекта

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,9 +2,7 @@ import { Injectable } from '@nestjs/common'
 
 @Injectable()
 export class AppService {
-  getHello(): { message: string } {
-    return {
-      message: 'Привет! Транспорт настроен',
-    }
+  getHello() {
+    return null
   }
 }
